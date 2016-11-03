5274bdde14eacced1919b5cc2f726e279a29d748
Joaquim Silva Neto <joaquim.sneto@sp.senac.br>
2016-11-03 17:11:07 -0200

    Finalizado burndown por semana

--- front-end/front/js/modules/dashboard/services/dashboard.service.js
+++ front-end/front/js/modules/dashboard/services/dashboard.service.js
@@ -31,25 +31,108 @@ function DashboardService(requestApiService, ModuloService) {
     return totalPontos;
   }
 
-  function visualizacaoPorPontos(burndown, modulo, periodo) {
+  function labelDia(data) {
+    var diaSemana = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'];
+
+    return diaSemana[data.getDay()] + " - " + data.getDate() + " / " + data.getMonth() + 1;
+  }
+
+  function visualizacaoPorPontosAndDias(burndown, modulo, periodo) {
     var dataInicio = new Date(modulo.dataInicio);
     var dataFim = new Date(modulo.dataFim);
+    var dataAtual = new Date();
     var intervalo = Math.round(intervaloDias(dataInicio, dataFim));
 
     var totalDePontos = totalPontos(modulo.funcionalidades);
     var valorIdeal = Math.round(totalDePontos / intervalo);
 
-    var resultadoSubtracao = totalDePontos;
+    var resultadoSubtracao = angular.copy(totalDePontos);
     var dataTrabalho = angular.copy(dataInicio);
-
+    
     burndown.esperado.push(angular.copy(resultadoSubtracao));
     for(var index = 0; index <= intervalo; index ++) {
+      // Label y
+      burndown.periodo.push(labelDia(dataTrabalho));
+      
       resultadoSubtracao = resultadoSubtracao - valorIdeal;
       burndown.esperado.push(angular.copy(resultadoSubtracao));
 
-      //function buscarTarefaPorData(tarefa) {
-        //var data
-      //}
+      // Busca as tarefas para o dia corrente
+      function buscarTarefaPorData(tarefa) {
+        if (tarefa.dataFim) {
+          var dataTarefa = new Date(tarefa.getDateFim);
+          return (dataTrabalho.getDate() === dataTarefa.getDate() && 
+                  dataTarefa.getMonth() === dataTrabalho.getMonth() &&
+                  tarefa.getStatus.codigo == 10);
+        } else {
+          return false;
+        }
+      }
+
+      var tarefaRealizadasNoDiaTrabalho = modulo.funcionalidades.filter(buscarTarefaPorData);
+      var pontosRealizados = totalPontos(tarefaRealizadasNoDiaTrabalho);
+
+      if (pontosRealizados > 0) {
+        totalDePontos = totalDePontos - pontosRealizados;
+        burndown.andamento.push(totalDePontos);
+      } else {
+        if(dataTrabalho <= dataAtual) {
+          burndown.andamento.push(totalDePontos);
+        }
+      }
+
+      // Acrescena um dia de trabalho
+      dataTrabalho.setDate(dataTrabalho.getDate + 1);
+    } 
+  }
+
+   function visualizacaoPorPontosAndSemanas(burndown, modulo, periodo) {
+    var dataInicio = new Date(modulo.dataInicio);
+    var dataFim = new Date(modulo.dataFim);
+    var dataAtual = new Date();
+    var intervalo = Math.round(intervaloDias(dataInicio, dataFim));
+
+    var totalDePontos = totalPontos(modulo.funcionalidades);
+    // ideal por semana
+    var valorIdeal = Math.round(totalDePontos / (intervalo / 7)) ;
+
+    var resultadoSubtracao = angular.copy(totalDePontos);
+    var dataTrabalho = angular.copy(dataInicio);
+    
+    burndown.esperado.push(angular.copy(resultadoSubtracao));
+    for(var index = 0, sem = 1; index <= intervalo; index = index + 6, sem ++) {
+      // Label y
+      burndown.periodo.push("Semana - " + sem);
+      
+      // final 
+      resultadoSubtracao = resultadoSubtracao - valorIdeal;
+
+      burndown.esperado.push(angular.copy(resultadoSubtracao));
+
+      // Busca as tarefas para o dia corrente
+      function buscarTarefaPorData(tarefa) {
+        if (tarefa.dataFim) {
+          var dataTarefa = new Date(tarefa.getDateFim);
+          return (dataTrabalho.getDate() >= dataTarefa.getDate() && tarefa.getStatus.codigo == 10);
+        } else {
+          return false;
+        }
+      }
+
+      var tarefaRealizadasNoDiaTrabalho = modulo.funcionalidades.filter(buscarTarefaPorData);
+      var pontosRealizados = totalPontos(tarefaRealizadasNoDiaTrabalho);
+
+      if (pontosRealizados > 0) {
+        totalDePontos = totalDePontos - pontosRealizados;
+        burndown.andamento.push(totalDePontos);
+      } else {
+        if(dataTrabalho <= dataAtual) {
+          burndown.andamento.push(totalDePontos);
+        }
+      }
+
+      // Acrescena um dia de trabalho
+      dataTrabalho.setDate(dataTrabalho.getDate + 7);
     } 
   }
 
@@ -63,11 +146,16 @@ function DashboardService(requestApiService, ModuloService) {
       console.log('retorno', retorno);
       var burndown = {
         esperado:  [],
-        andamento: []
+        andamento: [],
+        periodo: []
       };
 
-      if(visualizacao.value === "pontos") {
-        visualizacaoPorPontos(burndown, retorno, periodo);
+      if(visualizacao.value === "pontos" && periodo.value === "dias") {
+        visualizacaoPorPontosAndDias(burndown, retorno, periodo);
+      }
+
+      if(visualizacao.value === "pontos" && periodo.value === "semanas") {
+        visualizacaoPorPontosAndSemanas(burndown, retorno, periodo);
       }
 
       callback(burndown);
--- microservices/business-api/config/env/development.js
+++ microservices/business-api/config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   env:                 'development',
-  db:                  'mongodb://localhost/mas-tg',
+  db:                  'mongodb://35.160.139.101:3200/mas-tg',
   port:                4000,
   address:             'localhost',
   domain:              'localhost:4000',
